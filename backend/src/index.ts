05a2b987254db5ec6d6d392027a05e72df578872
bcomyn <bram.comyn@ugent.be>
2025-02-20 14:33:32 +0100

    feat: created empty Node.js/Express application and a Dockerfile for the backend

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+// Application entry point
+
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// TODO: implement backend application
+
+app.get('/', (req, res) => {
+  res.send("Hello, World!\n");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
