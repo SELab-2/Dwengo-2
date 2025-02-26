7e5e6bfb8ac50b671f9ed2702d19a74587e6a737
bcomyn <bram.comyn@ugent.be>
2025-02-26 20:19:05 +0100

    fix: test folders is now a sibling dir instead of a child dir of src

---
+++ backend/test/application/helpers.test.ts
@@ -0,0 +1,13 @@
+import { Request as ExpressRequest, Response as ExpressResponse } from 'express';
+import { requestFromExpress, requestToExpress, responseFromExpress, responseToExpress } from "../../src/application/helpers";
+import { Request, Response, HttpMethod } from "../../src/application/types";
+
+describe("Request Objects", (): void => {
+  it.todo("can be converted from Express Request objects");  
+  it.todo("can be converted to Express Request objects");
+});
+
+describe("Response Objects", (): void => {
+  it.todo("can be converted from Express Response objects");
+  it.todo("can be converted to Express Response objects");
+});
