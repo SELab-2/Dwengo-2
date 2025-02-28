fa01d308f01359bdced830f40f51ca66be2a1c41
lennertdr <lennertderouck@gmail.com>
2025-02-28 13:05:39 +0100

    lazy loading - get rid of interfaces

--- backend/src/core/entities/student.ts
+++ backend/src/core/entities/student.ts
@@ -1,13 +1,14 @@
 import { ClassInterface } from "./classInterface";
-import { IStudent } from "./studentInterface";
+import { User } from "./userInterface";
 
-export class Student implements IStudent {
+export class Student extends User {
     public constructor(
         public readonly email: string,
         public first_name: string,
         public family_name: string,
         public readonly password_hash: string,
-        public classes: ClassInterface[],
         public readonly id?: string,
-    ) {}
+    ) {
+        super(email, first_name, family_name, password_hash, id);
+    }
 }
--- backend/src/core/entities/userInterface.ts
+++ backend/src/core/entities/userInterface.ts
@@ -1,10 +1,9 @@
-import { ClassInterface } from "./classInterface";
-
-export interface IUser {
-    // Necessary variables
-    id?: string;    // User's id
-    email: string;  // User's email
-    first_name: string;     // User's first name
-    family_name: string;    // User's family name
-    password_hash: string;  // User's hashed password
+export abstract class User {
+    constructor(
+        public email: string,
+        public first_name: string,
+        public family_name: string,
+        protected password_hash: string,
+        public id?: string
+    ) {}
 }
