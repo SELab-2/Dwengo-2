08a2c88592ac4a45e7756712ec8da8adccb019e8
lennertdr <lennertderouck@gmail.com>
2025-02-28 14:47:25 +0100

    thread question

---
+++ backend/src/core/entities/threadQuestion.ts
@@ -0,0 +1,10 @@
+export enum Visibility {Public, Private}
+
+export class ThreadQuestion {
+    constructor(
+        public readonly senderId: string,
+        public readonly createdAt: Date,
+        public readonly threadId: string,
+        public content: string,
+    ){}
+}
