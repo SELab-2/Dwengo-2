5615c526cf4b0f9a27140a5e96abe754abd6945a
Maarten Lambrecht <maalambr.lambrecht@ugent.be>
2025-02-28 17:42:32 +0100

    Feat: implementation of getStudent + tests, changes to usecase so it returns generic type

---
+++ backend/test/application/core/use-cases/getStudent.test.ts
@@ -0,0 +1,40 @@
+import { GetSudent } from "../../../../src/core/use-cases/student/getStudent";
+import { StudentRepositoryInterface } from "../../../../src/core/repositories/studentRepositoryInterface";
+import { Student } from "../../../../src/core/entities/student";
+
+describe("getStudent Use Case", () => {
+  let getStudentUseCase: GetSudent;
+  let mockStudentRepository: jest.Mocked<StudentRepositoryInterface>;
+
+  beforeEach(() => {
+    mockStudentRepository = {
+      getStudent: jest.fn(), // Mock DB function
+    } as unknown as jest.Mocked<StudentRepositoryInterface>;
+
+    getStudentUseCase = new GetSudent(mockStudentRepository);
+  });
+
+  test("Should return student if found", async () => {
+    const student: Student = {
+      id: "1",
+      email: "test@student.com",
+      first_name: "John",
+      family_name: "Doe",
+      password_hash: "hashedpassword123",
+    };
+
+    mockStudentRepository.getStudent.mockResolvedValue(student);
+    const result = await getStudentUseCase.execute("1");
+
+    expect(result).toEqual(student);
+    expect(mockStudentRepository.getStudent).toHaveBeenCalledWith("1");
+  });
+
+  test("Should return null if student is not found", async () => {
+    mockStudentRepository.getStudent.mockResolvedValue(null);
+    const result = await getStudentUseCase.execute("999");
+
+    expect(result).toBeNull();
+    expect(mockStudentRepository.getStudent).toHaveBeenCalledWith("999");
+  });
+});
