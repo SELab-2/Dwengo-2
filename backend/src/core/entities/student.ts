b5710c88ee200f017fc88899f368b11c8015d332
lennertdr <lennertderouck@gmail.com>
2025-02-28 12:36:15 +0100

    nullable field for id

--- backend/src/core/entities/student.ts
+++ backend/src/core/entities/student.ts
@@ -3,11 +3,11 @@ import { IStudent } from "./studentInterface";
 
 export class Student implements IStudent {
     public constructor(
-        public readonly id: string,
         public readonly email: string,
         public first_name: string,
         public family_name: string,
         public readonly password_hash: string,
         public classes: ClassInterface[],
+        public readonly id?: string,
     ) {}
 }
--- backend/src/core/entities/userInterface.ts
+++ backend/src/core/entities/userInterface.ts
@@ -2,8 +2,8 @@ import { ClassInterface } from "./classInterface";
 
 export interface IUser {
     // Necessary variables
-    id: string;     // User's id
-    email: string;  // User's email
+    id?: string;             // User's id
+    email: string;          // User's email
     first_name: string;     // User's first name
     family_name: string;    // User's family name
     password_hash: string;  // User's hashed password
