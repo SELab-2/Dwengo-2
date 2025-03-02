cabeb70edef24c1269f10eb2bd1e11de455c0c20
lennertdr <lennertderouck@gmail.com>
2025-03-02 09:43:30 +0100

    private properties, getters & setters

--- backend/src/core/entities/class.ts
+++ backend/src/core/entities/class.ts
@@ -1,8 +1,36 @@
 export class Class {
     public constructor(
-        public name: string,
-        public description: string,
-        public targetAudience: string,  // Who is this class directed to (8th grade, home schooling, ...)
-        public id?: string,
+        private name: string,
+        private description: string,
+        private targetAudience: string,  // Who is this class directed to (8th grade, home schooling, ...)
+        private id?: string,
     ){}
+
+    public getName():string{
+        return this.name;
+    }
+    public setName(name:string):void{
+        this.name = name;
+    }
+
+    public getDescription():string{
+        return this.description;
+    }
+    public setDescription(description:string):void{
+        this.description = description;
+    }
+
+    public getTargetAudience():string{
+        return this.targetAudience;
+    }
+    public setTargetAudience():void{
+        this.targetAudience = this.targetAudience;
+    }
+
+    public getId():string|undefined{
+        return this.id;
+    }
+    public setId(id:string):void{
+        this.id = id;
+    }
 }
