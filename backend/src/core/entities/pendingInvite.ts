07147906c420299817af4f2d4cd6c2f039652af4
lennertdr <lennertderouck@gmail.com>
2025-03-02 09:57:23 +0100

    No inviteId needed, requester and class id is enough

--- backend/src/core/entities/pendingInvite.ts
+++ backend/src/core/entities/pendingInvite.ts
@@ -2,7 +2,6 @@ export class PendingInvite {
     public constructor(
         private readonly requestId: string,
         private readonly classId: string,
-        private inviteId?: string,
     ){}
 
     public getRequestId(): string {
@@ -11,12 +10,5 @@ export class PendingInvite {
 
     public getClassId(): string {
         return this.classId;
-    }
-
-    public getInviteId(): string | undefined {
-        return this.inviteId;
-    }
-    public setInviteId(inviteId: string) {
-        this.inviteId = inviteId;
     }
 }
