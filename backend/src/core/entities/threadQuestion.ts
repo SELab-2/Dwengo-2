0f5e193ad1ec8eb5cdcbb8dd431f166d6919155d
lennertdr <lennertderouck@gmail.com>
2025-03-02 10:09:06 +0100

    a more typescript-style approach to getters and setters

--- backend/src/core/entities/threadQuestion.ts
+++ backend/src/core/entities/threadQuestion.ts
@@ -1,8 +1,27 @@
 export class ThreadQuestion {
     constructor(
-        public readonly senderId: string,
-        public readonly createdAt: Date,
-        public readonly threadId: string,
-        public content: string,
+        private readonly _senderId: string,
+        private readonly _createdAt: Date,
+        private readonly _threadId: string,
+        private _content: string,
     ){}
+
+    // Getters
+    public get senderId(): string {
+        return this._senderId;
+    }
+    public get createdAt(): Date {
+        return this._createdAt;
+    }
+    public get threadId(): string {
+        return this._threadId;
+    }
+    public get content(): string {
+        return this._content;
+    }
+
+    // Setter
+    public set content(newContent: string) {
+        this._content = newContent;
+    }
 }
