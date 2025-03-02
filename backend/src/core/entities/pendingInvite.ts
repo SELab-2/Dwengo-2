94397eab3e3d9ef6341d59b8a066a63eb3a37ebb
lennertdr <lennertderouck@gmail.com>
2025-03-02 09:49:43 +0100

    private properties with getters&setters

--- backend/src/core/entities/pendingInvite.ts
+++ backend/src/core/entities/pendingInvite.ts
@@ -1,7 +1,28 @@
 export class PendingInvite {
     public constructor(
-        public requestId: string,
-        public classId: string,
-        public inviteId?: string,
+        private requestId: string,
+        private classId: string,
+        private inviteId?: string,
     ){}
+
+    public getRequestId(): string {
+        return this.requestId;
+    }
+    public setRequestId(requestId: string) {
+        this.requestId = requestId;
+    }
+
+    public getClassId(): string {
+        return this.classId;
+    }
+    public setClassId(classId: string) {
+        this.classId = classId;
+    }
+
+    public getInviteId(): string | undefined {
+        return this.inviteId;
+    }
+    public setInviteId(inviteId: string) {
+        this.inviteId = inviteId;
+    }
 }
