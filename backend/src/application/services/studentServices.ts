c6eaa9d42e3b86f140e1b47fcf643fc7906fb9ee
AdrienVL <adrien.vanlancker@ugent.be>
2025-03-05 17:16:44 +0000

    fix: student refactor

--- backend/src/application/services/studentServices.ts
+++ backend/src/application/services/studentServices.ts
@@ -1,6 +1,8 @@
 import { Service } from './service';
 import { Student } from '../../core/entities/student';
-import { UUID } from 'crypto';
+import { GetStudent } from '../../core/use-cases/student/getStudent';
+import { DeleteStudent } from '../../core/use-cases/student/deleteStudent';
+import { UpdateStudent } from '../../core/use-cases/student/updateStudent';
 
 /**
  * Collection of services for student-related operations.
@@ -14,7 +16,7 @@ import { UUID } from 'crypto';
  */
 export class GetStudentService extends Service<Student> {
   constructor(get: GetStudent) { super({ get }); }
-  execute(studentId: UUID) { return this.usecases.get.execute(studentId); }
+  // TODO: implement the execute function
 }
 
 /**
@@ -22,7 +24,7 @@ export class GetStudentService extends Service<Student> {
  */
 export class UpdateStudentService extends Service<Student> {
   constructor(update: UpdateStudent) { super({ update }); }
-  execute(studentId: UUID, data: object) { return this.usecases.update.execute(studentId, data); }
+  // TODO: implement the execute function
 }
 
 /**
@@ -30,21 +32,21 @@ export class UpdateStudentService extends Service<Student> {
  */
 export class DeleteStudentService extends Service<void> {
   constructor(remove: DeleteStudent) { super({ remove }); }
-  execute(studentId: UUID) { return this.usecases.remove.execute(studentId); }
+  // TODO: implement the execute function
 }
 
 /**
  * Service for removing a student from a class
  */
 export class RemoveStudentFromClassService extends Service<void> {
-  constructor(classRemove: RemoveStudentFromClass) { super({ classRemove }); }
-  execute(studentId: UUID, classId: UUID) { return this.usecases.classRemove.execute(studentId, classId); }
+  // TODO: implement constructor with RemoveStudentFromClass use case
+  // TODO: implement the execute function
 }
 
 /**
  * Service for removing a student from a group
  */
 export class RemoveStudentFromGroupService extends Service<void> {
-  constructor(groupRemove: RemoveStudentFromGroup) { super({ groupRemove }); }
-  execute(studentId: UUID, groupId: UUID) { return this.usecases.groupRemove.execute(studentId, groupId); }
+  // TODO: implement constructor with RemoveStudentFromGroup use case
+  // TODO: implement the execute function
 }
