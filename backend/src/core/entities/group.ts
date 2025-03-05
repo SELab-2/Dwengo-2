0eccbb82221dffe05f32305c366cbe354f76a9e4
robbevdk <robbe.vandekeere@ugent.be>
2025-03-05 13:30:26 +0100

    implement group entity & remove interface
    includes a list of group member ids

---
+++ backend/src/core/entities/group.ts
@@ -0,0 +1,29 @@
+export class Group {
+    public constructor(
+        private _classId: string,
+        private _members: string[],
+        private _id?: string,
+    ){}
+
+    // Getters
+    public get classId():string{
+        return this._classId;
+    }
+    public get members():string[]{
+        return this._members;
+    }
+    public get id():string|undefined{
+        return this._id;
+    }
+
+    // Setters
+    public set classId(newClassId:string){
+        this._classId = newClassId;
+    }
+    public set members(newMembers:string[]){
+        this._members = newMembers;
+    }
+    public set id(newId:string){
+        this._id = newId;
+    }
+}
