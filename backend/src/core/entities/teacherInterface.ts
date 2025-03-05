4d7065af0715a14ebed628113c79972e31e06849
robbevdk <robbe.vandekeere@ugent.be>
2025-03-05 13:03:54 +0100

    Delete teacherInterface.ts
    remove teacherInterface

--- backend/src/core/entities/teacherInterface.ts
+++
@@ -1,9 +0,0 @@
-import { ClassInterface } from "./class_interface";
-import { UserInterface } from "./user_interface";
-
-export interface TeacherInterface extends UserInterface{
-    // Optional variables
-    name_school?: string;   // Teacher's school
-
-    create_class: () => ClassInterface;
-}
