9c4d9085f70ba4fec1a6d4a8636cecae97dfe617
Bram Comyn <bram.comyn@ugent.be>
2025-03-05 15:39:57 +0100

    feat: implemented controller and service for invites

--- backend/src/application/controllers/pendingInviteController.ts
+++ backend/src/application/controllers/pendingInviteController.ts
@@ -1,4 +1,6 @@
 import { Controller } from './controllerExpress';
+import { Request, HttpMethod, RouteHandlers } from '../types';
+import * as PendingInviteServices from '../services/pendingInviteServices';
 
 /**
  * Controller responsible for pending invite-related API endpoints including CRUD operations
@@ -9,4 +11,26 @@ import { Controller } from './controllerExpress';
  * - DELETE /invites/:id - Delete invite
  * - POST /invites - Create new invite
  */
-export class PendingInviteController extends Controller {}
+export class PendingInviteController extends Controller {
+  constructor(
+    get: PendingInviteServices.Get,
+    userGet: PendingInviteServices.UserGet,
+    remove: PendingInviteServices.Delete,
+    create: PendingInviteServices.Create
+  ) {
+    const handlers: RouteHandlers = {
+      [HttpMethod.GET]: [
+        { parent: 'users', hasId: true, hasParentId: true, handler: (req: Request) => this.getOne(req) },
+        { parent: 'users', hasId: false, hasParentId: true, handler: (req: Request) => this.getMany(req) },
+      ],
+      [HttpMethod.DELETE]: [
+        { hasId: true, hasParentId: false, handler: (req: Request) => this.delete(req) },
+      ],
+      [HttpMethod.POST]: [
+        { hasId: false, hasParentId: false, handler: (req: Request) => this.create(req) },
+      ],
+    };
+
+    super({ get, userGet, remove, create }, handlers);
+  }
+}
--- backend/src/application/services/pendingInviteServices.ts
+++ backend/src/application/services/pendingInviteServices.ts
@@ -1,24 +1,26 @@
-import { Service } from './service';
-import { UUID } from 'crypto';
-
-// TODO: add other imports for usecases
+import { Service } from "./service";
+import { PendingInvite } from "../../core/entities/pendingInvite";
+import { GetInvite } from "../../core/use-cases/pending_invite/getInvite";
+import { GetUserInvite } from "../../core/use-cases/pending_invite/getUserInvite";
+import { DeleteInvite } from "../../core/use-cases/pending_invite/deleteInvite";
+import { CreateInvite } from "../../core/use-cases/pending_invite/createInvite";
 
 export class Get extends Service<PendingInvite> {
-  constructor(get: GetPendingInvite) { super({ get }); }
-  execute(pendingInviteId: UUID) { return this.usecases.get.execute(pendingInviteId); }
+  constructor(get: GetInvite) { super({ get }); }
+  // TODO: implement the execute function
 }
 
-export class UserGet extends Service<PendingInvite[]> {
-  constructor(get: GetUserPendingInvites) { super({ get }); }
-  execute(userId: UUID, page: number, size: number) { return this.usecases.get.execute(userId, page, size); }
+export class UserGet extends Service<PendingInvite> {
+  constructor(userGet: GetUserInvite) { super({ userGet }); }
+  // TODO: implement the execute function
 }
 
-export class Delete extends Service<PendingInvite[]> {
-  constructor(remove: DeletePendingInvite) { super({ remove }); }
-  execute(pendingInviteId: UUID) { return this.usecases.remove.execute(pendingInviteId); }
+export class Delete extends Service<PendingInvite> {
+  constructor(remove: DeleteInvite) { super({ remove }); }
+  // TODO: implement the execute function
 }
 
 export class Create extends Service<PendingInvite> {
-  constructor(create: CreatePendingInvite) { super({ create }); }
-  execute(data: object) { return this.usecases.create.execute(data); }
+  constructor(create: CreateInvite) { super({ create }); }
+  // TODO: implement the execute function
 }
