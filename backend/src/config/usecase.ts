a2dd325e4a29e710191a065c8976728b94507cee
thomasdejaeghere <thomas.dejaeghere@ugent.be>
2025-03-07 13:50:37 +0100

    fixed linting error by using object instead of Object

--- backend/src/config/usecase.ts
+++ backend/src/config/usecase.ts
@@ -3,6 +3,6 @@ export interface UseCase<Type, ReturnType> {
 }
 
 export interface UseCaseParams<Type>{
-  fromObject: (input: Object) => Type,
-  toObject: (input: Type) => Object
+  fromObject: (input: object) => Type,
+  toObject: (input: Type) => object
 }
