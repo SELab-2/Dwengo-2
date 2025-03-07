8154f96f7de582f1f03378335735c10466b20cdc
Briek <briek.rombaut@ugent.be>
2025-03-07 10:57:23 +0100

    kleine aanpassing aan student entity

--- backend/src/core/entities/student.ts
+++ backend/src/core/entities/student.ts
@@ -1,14 +1,23 @@
-import { User } from "./userInterface";
+import { User } from "./user";
 
 export class Student extends User {
-    public constructor(
-        email: string,
-        first_name: string,
-        last_name: string,
-        password_hash: string,
-        id?: string,
-    ) {
-        super(email, first_name, last_name, password_hash, id);
-    }
 
+    constructor(
+        email: string, 
+        first_name: string, 
+        last_name: string, 
+        password_hash: string, 
+        name_school?: string,
+        id?: string
+    ) {
+        super(
+            email,
+            first_name,
+            last_name,
+            password_hash,
+            name_school,
+            id
+        );
+    }
+    
 }
--- backend/src/core/entities/teacher.ts
+++ backend/src/core/entities/teacher.ts
@@ -5,7 +5,7 @@ export class Teacher extends User {
     constructor(
         email: string, 
         first_name: string, 
-        family_name: string, 
+        last_name: string, 
         password_hash: string, 
         name_school?: string,
         id?: string
@@ -13,7 +13,7 @@ export class Teacher extends User {
         super(
             email,
             first_name,
-            family_name,
+            last_name,
             password_hash,
             name_school,
             id
