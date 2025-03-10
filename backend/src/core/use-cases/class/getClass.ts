7cf6c21e31bf02b7272d9100e5da8dd0ecebe29e
lennertdr <lennertderouck@gmail.com>
2025-03-10 12:42:52 +0100

    get class service (still as use case but will change after refactor)

--- backend/src/core/use-cases/class/getClass.ts
+++ backend/src/core/use-cases/class/getClass.ts
@@ -1,9 +1,21 @@
 import { UseCase } from "../../../config/useCase";
+import { Class } from "../../entities/class";
+import { IClassRepository } from "../../repositories/classRepositoryInterface";
 
-export class GetClass implements UseCase<undefined, undefined> {
-  constructor() {}
 
-  async execute(input: undefined): Promise<undefined> {
-    //TODO
+//TODO - change after refactor
+
+
+export class GetClassByClassId implements UseCase<string, Class> {
+  constructor(private classRepository: IClassRepository) {}
+
+  /**
+   * Gets a class from the DB.
+   * @param input ID of the class to get from the DB.
+   * @returns the class with the given id.
+   * @throws Error if the class could not be found.
+   */
+  async execute(input: string): Promise<Class> {
+    return this.classRepository.getClassById(input);
   }
 }
