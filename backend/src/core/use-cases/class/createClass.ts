52849c609946f28e29e67e664a5f04de0463833d
lennertdr <lennertderouck@gmail.com>
2025-03-10 20:14:05 +0100

    create class

--- backend/src/core/use-cases/class/createClass.ts
+++ backend/src/core/use-cases/class/createClass.ts
@@ -3,6 +3,12 @@ import { ClassBaseUseCase } from "./baseClassUseCase";
 
 export class CreateClass extends ClassBaseUseCase<Class, Class> {
 
+  /**
+   * Creates a database entry for a newly made class.
+   * @param input the class object without an id.
+   * @returns the class object with the id set.
+   * @throws {DatabaseError} when the creation was failed.
+   */
   async execute(input: Class): Promise<Class> {
     return this.classRepository.createClass(input);
   }
