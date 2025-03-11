f4ca79c90e23d8dda8fa3e27f02d33378208ac00
Maarten Lambrecht <maalambr.lambrecht@ugent.be>
2025-03-11 16:20:07 +0100

    Feat: Implement service to remove a teacher from a class

--- backend/src/core/services/student/removeStudentFrom.ts
+++ backend/src/core/services/student/removeStudentFrom.ts
@@ -1,27 +1,16 @@
 import { Service, ServiceParams } from '../../../config/service';
 import { IStudentRepository } from '../../repositories/studentRepositoryInterface';
 
-interface RemoveStudentID {
-  studentId: string;
-  otherId: string;
-}
-
 // Class to be used by execute methode
 export class RemoveStudentFromParams implements ServiceParams {
-  private studentId: string;
-  private otherId: string;
-  constructor(studentId: string, otherId: string) {
-    this.studentId = studentId;
-    this.otherId = otherId;
+  constructor(private _studentId: string, private _otherId: string) {}
+
+  get studentId() {
+    return this._studentId;
   }
 
-  /**
-   * Function that gives back an object containing studentId and otherId.
-   *
-   * @returns object containing the studentId and otherId to remove student.
-   */
-  public fromObject(): RemoveStudentID {
-    return { studentId: this.studentId, otherId: this.otherId };
+  get otherId() {
+    return this._otherId;
   }
 }
 
@@ -47,8 +36,7 @@ export abstract class RemoveStudentFrom
    *
    */
   async execute(input: RemoveStudentFromParams): Promise<object> {
-    const id: RemoveStudentID = input.fromObject();
-    await this.removeStudent(id.studentId, id.otherId);
+    await this.removeStudent(input.studentId, input.otherId);
     return {};
   }
 }
--- backend/src/core/services/teacher/removeTeacherFromClass.ts
+++ backend/src/core/services/teacher/removeTeacherFromClass.ts
@@ -1,9 +1,27 @@
-import { Service, ServiceParams } from "../../../config/service";
+import { Service, ServiceParams } from '../../../config/service';
+import { ITeacherRepository } from '../../repositories/teacherRepositoryInterface';
 
-export class RemoveTeacherFromClass implements Service<ServiceParams> {
+export class RemoveTeacherFromClassParams implements ServiceParams {
+  constructor(private _teacherId: string, private _classId: string) {}
+  get teacherId() {
+    return this._teacherId;
+  }
 
-  async execute(input: ServiceParams): Promise<object> {
-    //TODO: Implement this method
+  get classId() {
+    return this._classId;
+  }
+}
+
+export class RemoveTeacherFromClass
+  implements Service<RemoveTeacherFromClassParams>
+{
+  constructor(private teacherRepository: ITeacherRepository) {}
+
+  async execute(input: RemoveTeacherFromClassParams): Promise<object> {
+    await this.teacherRepository.deleteTeacherFromClass(
+      input.teacherId,
+      input.classId,
+    );
     return {};
   }
 }
