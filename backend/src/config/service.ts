26967a6bbcd171733904d4e3953aae1cbf7b50f2
lennertdr <lennertderouck@gmail.com>
2025-03-11 11:51:31 +0100

    local approved changes to service

--- backend/src/config/service.ts
+++ backend/src/config/service.ts
@@ -1,4 +1,3 @@
-
 export interface ServiceParams {
   //TODO can we abstract the fromObject method in services to for a Params as abstracted class or interface?
   // fromObject: (repository: any) => Promise<any>;
@@ -6,9 +5,9 @@ export interface ServiceParams {
 }
 
 //TODO if application layer always asks for an object in return to we need to specify ReturnType?
-export interface Service<T extends ServiceParams, ReturnType> {
-  execute: (input: T) => Promise<ReturnType>;
+export interface Service<T extends ServiceParams> {
+  execute: (input: T) => Promise<object>;
 }
 
 //TODO can we abstract the params type in services to for a Params as abstracted class or interface?
-export type Services = Record<string, any>;
+export type Services = Record<string, Service<ServiceParams>>;
