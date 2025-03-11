328bc0ed80dc97f6ac8448c7ffa468faba0d0da0
Briek <briek.rombaut@ugent.be>
2025-03-11 09:54:12 +0100

    tests

---
+++ backend/test/infrastructure/repositories/submissionRepositoryTypeORM.test.ts
@@ -0,0 +1,81 @@
+import { StatusType, Submission } from "../../../src/core/entities/submission";
+import { IDatasourceSubmission } from "../../../src/infrastructure/database/data/data_sources/datasourceSubmissionInterface";
+import { IDatasourceFactory } from "../../../src/infrastructure/database/data/data_sources/datasourceFactoryInterface";
+import { IDatasource } from "../../../src/infrastructure/database/data/data_sources/datasourceInterface";
+
+describe("SubmissionRepositoryTypeORM", () => {
+
+    let datasourceMock: IDatasource;
+    let datasourceFactoryMock: IDatasourceFactory;
+    let submission: Submission;
+
+    let datasourceSubmission: IDatasourceSubmission;
+
+    beforeEach(() => {
+        datasourceMock = {
+            getDatasourceTeacher: jest.fn(),
+            getDatasourceClass: jest.fn(),
+            getDatasourceJoinRequest: jest.fn(),
+            getDatasourceAssignment: jest.fn(),
+            getDatasourceSubmission: jest.fn(),
+        };
+        datasourceFactoryMock = {
+            createDatasource: jest.fn(() => datasourceMock),
+        };
+
+        datasourceSubmission = {
+            createSubmission: jest.fn(() => Promise.resolve(submission)),
+            getSubmissionById: jest.fn(() => Promise.resolve(submission)),
+            getSubmissionsByClassId: jest.fn(() => Promise.resolve(submission)),
+            getSubmissionsByLearningPathId: jest.fn(() => Promise.resolve([submission, submission])),
+            deleteSubmissionById: jest.fn()
+        } as any;
+
+        // Mock submission
+        submission = new Submission(
+            "studentId",
+            "assignmentId",
+            "learningObjectId",
+            new Date("01/04/2025"),
+            new Buffer("OAPFOJIHUHZDOJDJ"),
+            StatusType.NOT_ACCEPTED,
+        );
+
+    });
+
+    test("create", async () => {
+        // Call function from repository
+        await datasourceSubmission.create(submission);
+        
+        expect(datasourceSubmission.create).toHaveBeenCalledTimes(1);
+        expect(datasourceSubmission.create).toHaveBeenCalledWith(submission);
+    });
+
+    test("getById", async () => {
+        // Call function from repository
+        await datasourceSubmission.getById(submission.id!);
+
+        expect(datasourceSubmission.getById).toHaveBeenCalledTimes(1);
+        expect(datasourceSubmission.getById).toHaveBeenCalledWith(submission.id!);
+    });
+
+
+    test("update", async () => {
+        // Call function from repository
+        submission.status = StatusType.ACCEPTED;
+        const updatedSubmission : Submission = await datasourceSubmission.update(submission);
+
+        expect(datasourceSubmission.update).toHaveBeenCalledTimes(1);
+        expect(datasourceSubmission.update).toHaveBeenCalledWith(submission);
+        expect(updatedSubmission.status).toEqual(submission.status);
+    });
+
+    test("delete", async () => {
+        // Call function from repository
+        await datasourceSubmission.delete(submission);
+
+        expect(datasourceSubmission.delete).toHaveBeenCalledTimes(1);
+        expect(datasourceSubmission.delete).toHaveBeenCalledWith(submission.id!);
+    });
+
+});
