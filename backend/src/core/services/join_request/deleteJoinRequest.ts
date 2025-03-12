6a59ab77c96354f076b46870ce73f407c52270f6
Maarten Lambrecht <maalambr.lambrecht@ugent.be>
2025-03-12 16:13:54 +0100

    Feat: implementation of service to delete a join request + tests

--- backend/src/core/services/join_request/deleteJoinRequest.ts
+++ backend/src/core/services/join_request/deleteJoinRequest.ts
@@ -1,9 +1,19 @@
-import { Service, ServiceParams } from "../../../config/service";
+import { Service, ServiceParams } from '../../../config/service';
+import { IJoinRequestRepository } from '../../repositories/joinRequestRepositoryInterface';
 
-export class DeleteJoinRequest implements Service<ServiceParams> {
-  constructor() {}
+export class DeleteJoinRequestParams implements ServiceParams {
+  constructor(private _id: string) {}
 
-  async execute(input: ServiceParams): Promise<object> {
+  get id(): string {
+    return this._id;
+  }
+}
+
+export class DeleteJoinRequest implements Service<DeleteJoinRequestParams> {
+  constructor(private joinRequestRepository: IJoinRequestRepository) {}
+
+  async execute(input: DeleteJoinRequestParams): Promise<object> {
+    await this.joinRequestRepository.deleteJoinRequestById(input.id);
     return {};
   }
 }
