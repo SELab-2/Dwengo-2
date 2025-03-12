4a1e035ad4e96ea2f111e9cfc86137303c3bb83d
thomasdejaeghere <thomas.dejaeghere@ugent.be>
2025-03-12 14:01:54 +0100

    fix lints

--- backend/src/core/services/group/getAssignmentGroups.ts
+++ backend/src/core/services/group/getAssignmentGroups.ts
@@ -3,5 +3,5 @@ import { Service, ServiceParams } from '../../../config/service';
 export class GetAssignmentGroups implements Service<ServiceParams> {
     async execute(input: ServiceParams): Promise<object>{
         return {};
-    };	
+    }	
 }
