07d397cee6fc819877b4b3295be6a1b351b7224d
AdrienVL <adrien.vanlancker@ugent.be>
2025-03-19 23:43:44 +0100

    fix: shouldn't be shared by all routes

--- backend/src/config/setupMiddleware.ts
+++ backend/src/config/setupMiddleware.ts
@@ -1,18 +1,15 @@
 import cors from "cors";
 import express from "express";
 import { Express } from "express";
-import authenticationMiddleware from "../application/middleware/authenticationMiddleware";
 
 /**
  * Setup all middleware for the application.
  *
  * Sets up the cors and express.json middleware,
- * as well as our own authorization middleware.
  *
  * @param app The epxress app
  */
 export function setupMiddleware(app: Express): void {
     app.use(cors());
     app.use(express.json());
-    app.use(authenticationMiddleware);
 }
