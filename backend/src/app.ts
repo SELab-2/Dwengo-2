5fce3b49d4c0f72a9704acf1327ad7b6b7cc6b4e
AdrienVL <adrien.vanlancker@ugent.be>
2025-03-19 23:41:21 +0100

    fix: some moving

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,10 @@
+import dotenv from "dotenv";
 import express from "express";
-
 import { setupMiddleware } from "./config/setupMiddleware";
 import { setupRoutes } from "./config/setupRoutes";
 import { setupServer } from "./config/setupServer";
 
+dotenv.config();
 const app = express();
 setupMiddleware(app);
 setupRoutes(app);
--- backend/src/config/setupServer.ts
+++ backend/src/config/setupServer.ts
@@ -1,12 +1,6 @@
-import dotenv from "dotenv";
 import { Express } from "express";
-import { services } from "./services";
-import { ChallengeManager } from "../application/challenge";
-
-export const challengeManager = new ChallengeManager(services.users.get);
 
 export const setupServer = (app: Express) => {
-    dotenv.config();
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log(`Server is running at http://localhost:${PORT}`);
