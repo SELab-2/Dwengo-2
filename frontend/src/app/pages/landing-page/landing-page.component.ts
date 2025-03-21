9464be98d10063926da600eb9d1b5801c245de16
brentjan <brentjan.janssens@ugent.be>
2025-03-21 17:54:03 +0100

    feat: basic landing page

--- frontend/src/app/pages/landing-page/landing-page.component.ts
+++ frontend/src/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'landing-page',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [ 
+    CommonModule,
+
+    // For routing to other pages
+    // Based on https://angular.dev/guide/routing/common-router-tasks
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.less'
 })
