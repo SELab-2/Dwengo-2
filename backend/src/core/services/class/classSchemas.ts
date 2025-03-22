7c64150af74d4f20852d355610f3e1ecb1414e81
AdrienVL <adrien.vanlancker@ugent.be>
2025-03-22 22:31:59 +0100

    fix: remark that needs to be addressed

--- backend/src/core/services/class/classSchemas.ts
+++ backend/src/core/services/class/classSchemas.ts
@@ -1,5 +1,15 @@
 import { z } from "zod";
 
+// TODO - Move different get-types to a single schema with optionals and
+// - a zod refine that deterimes if at least one is defined
+// - all those that get with something different than a UUID or a unique
+// - - vaue field should be routed to a service that possibly gets multiple
+// - - entities that match, so a service like or equal to the one of
+// - - getAllClasses
+// - - example query: /users/:idParent/classes?name=cliodynamics
+// - these also don't really follow the consistent style that the
+// - - other schemas of group, users, assignments follow
+
 /**
  * Schemas for class services
  */
