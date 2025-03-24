d0d9fef3b0f2df5a7a8c5664543e918b1f57d977
AdrienVL <adrien.vanlancker@ugent.be>
2025-03-24 23:40:56 +0100

    feat: remove old route files

--- backend/src/application/routes/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./routesExpress";
-// export * from "./assignmentRoutes";
-// export * from "./classRoutes";
-// export * from "./groupRoutes";
-// export * from "./joinRequestRoutes";
-// export * from "./questionRoutes";
-// export * from "./messageRoutes";
-// export * from "../resources/usersRoutes";
-// export * from "../resources/authenticationRoutes";
