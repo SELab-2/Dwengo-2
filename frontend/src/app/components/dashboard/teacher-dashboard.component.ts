d44ea3d5af0ebd7b159973c673c689ee4c9442b2
lennertdr <lennertderouck@gmail.com>
2025-03-24 23:59:02 +0100

    nice graphs

--- frontend/src/app/components/dashboard/teacher-dashboard.component.ts
+++ frontend/src/app/components/dashboard/teacher-dashboard.component.ts
@@ -4,11 +4,50 @@ import { RouterLink } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { TeacherDashboardService } from '../../services/getClasses.service';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexYAxis,
+  ApexLegend,
+  ApexStroke,
+  ApexXAxis,
+  ApexFill,
+  ApexTooltip,
+  NgApexchartsModule,
+  ApexGrid,
+  ApexTitleSubtitle
+} from "ng-apexcharts";
+
+export type BarChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  fill: ApexFill;
+  tooltip: ApexTooltip;
+  stroke: ApexStroke;
+  legend: ApexLegend;
+};
+
+export type LineChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+  tooltip: ApexTooltip;
+};
 
 @Component({
   selector: 'app-teacher-dashboard',
   standalone: true,
-  imports: [RouterLink, CommonModule, HttpClientModule, MatIconModule],
+  imports: [RouterLink, CommonModule, HttpClientModule, MatIconModule, NgApexchartsModule],
   templateUrl: './teacher-dashboard.component.html',
   styleUrls: ['./teacher-dashboard.component.less'],
   providers: [TeacherDashboardService]
@@ -28,7 +67,117 @@ export class TeacherDashboardComponent implements OnInit {
     { assignment: "Science Project", question: "How do you calculate force?" }
   ];
 
-  constructor(private dashboardService: TeacherDashboardService) { }
+  public barChartOptions: Partial<BarChartOptions>;
+  public lineChartOptions: Partial<LineChartOptions>;
+
+  constructor(private dashboardService: TeacherDashboardService) {
+    // Again, this is mock data. 
+    this.barChartOptions = {
+      series: [
+        {
+          name: "Assignment Progress",
+          data: [76, 85, 99, 98, 87]
+        },
+        {
+          name: "Finished students",
+          data: [44, 55, 57, 56, 61]
+        }
+      ],
+      chart: {
+        type: "bar",
+        height: 350
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: "55%",
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ["transparent"]
+      },
+      xaxis: {
+        categories: [
+          "Maths",
+          "Physics",
+          "Biology",
+          "STEM",
+          "Literature"
+        ]
+      },
+      fill: {
+        opacity: 1
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return val + "%";
+          }
+        }
+      }
+    };
+
+    this.lineChartOptions = {
+      series: [
+        {
+          name: "Maths",
+          data: [10, 41, 35, 51, 49, 62, 69, 73, 86]
+        },
+        {
+          name: "Physics",
+          data: [5, 8, 12, 16, 28, 35, 43, 48, 54]
+        },
+        {
+          name: "STEM",
+          data: [15, 35, 42, 51, 58, 64, 72, 81, 95]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "straight"
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5
+        }
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep"
+        ]
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return val + " submissions (in total)";
+          }
+        }
+      }
+    };
+  }
 
   ngOnInit(): void { }
 
