4ab6709fef51a1fe752b9dd618dd634ae94403a6
Maarten Lambrecht <maalambr.lambrecht@ugent.be>
2025-03-25 23:17:51 +0100

    API: Change basic swagger for users + corresponding changes in code

--- backend/src/core/services/user/updateUser.ts
+++ backend/src/core/services/user/updateUser.ts
@@ -75,7 +75,7 @@ export class UpdateUser implements Service<UpdateUserInput> {
                 input.schoolName ?? oldUser.schoolName,
                 input.id,
             );
-            await this.studentRepository.updateStudent(updatedUser as Student);
+                return (await this.studentRepository.updateStudent(updatedUser as Student)).toObject();
         } else {
             updatedUser = new Teacher(
                 input.email ?? oldUser.email,
@@ -85,9 +85,7 @@ export class UpdateUser implements Service<UpdateUserInput> {
                 input.schoolName ?? oldUser.schoolName,
                 input.id,
             );
-            await this.teacherRepository.updateTeacher(updatedUser as Teacher);
+            return (await this.teacherRepository.updateTeacher(updatedUser as Teacher)).toObject();
         }
-
-        return {};
     }
 }
