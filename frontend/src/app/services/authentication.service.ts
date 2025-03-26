9e5e98ed4026e24fd7727c30a1306713c4c4ca6d
Bram Comyn <bram.comyn@ugent.be>
2025-03-26 14:38:48 +0100

    feat: create stubs in authentication service for token handling

--- frontend/src/app/services/authentication.service.ts
+++ frontend/src/app/services/authentication.service.ts
@@ -12,6 +12,7 @@ import {
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private tokenStored = false;
 
   constructor(private http: HttpClient) {}
 
@@ -21,6 +22,20 @@ export class AuthenticationService {
 
   register(user: UserRegistration): Observable<RegisterResponse> {
     return this.http.post<RegisterResponse>('http://localhost:3001/register', user);
+  }
+
+  storeToken(token: string): void {
+    // TODO: implement this function and check if an error would be wiser
+    this.tokenStored = true;
+  }
+
+  retrieveToken(): string | undefined {
+    // TODO: implement this function and check if an error would be wiser
+    if (this.tokenStored) {
+      return '';
+    }
+
+    return undefined;
   }
   
 }
