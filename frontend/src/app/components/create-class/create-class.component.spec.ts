49614139e9dcf160bc73071a8dc3e6e329eaebce
brentjan <brentjan.janssens@ugent.be>
2025-03-27 12:17:08 +0100

    chore: tests for create class component

--- frontend/src/app/components/create-class/create-class.component.spec.ts
+++ frontend/src/app/components/create-class/create-class.component.spec.ts
@@ -1,30 +1,62 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { provideHttpClient } from "@angular/common/http";
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { CreateClassComponent } from './create-class.component';
+import { ClassesService } from '../../services/classes.service';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
 
 describe('CreateClassComponent', () => {
   let component: CreateClassComponent;
-  let fixture: ComponentFixture<CreateClassComponent>;
+  let harness: RouterTestingHarness;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CreateClassComponent],
       providers: [
+        provideRouter([{ path: "**", component: CreateClassComponent }]),
+
         // https://angular.dev/guide/http/testing
+        ClassesService,
         provideHttpClient(),
         provideHttpClientTesting()
       ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(CreateClassComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    harness = await RouterTestingHarness.create();
+    component = await harness.navigateByUrl('/', CreateClassComponent);
+    harness.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a create class form', () => {
+    const createClassForm = harness.fixture.nativeElement.querySelector('form');
+    expect(createClassForm).toBeTruthy();
+  });
+
+  it('should have a class name input', () => {
+    const classNameInput = harness.fixture.nativeElement.querySelector('input[id="name"]');
+    expect(classNameInput).toBeTruthy();
+  });
+
+  it('should have a class description input', () => {
+    const classDescriptionInput = harness.fixture.nativeElement.querySelector('textarea[id="description"]');
+    expect(classDescriptionInput).toBeTruthy();
+  });
+
+  it('should have a class target audience input', () => {
+    const classTargetAudienceInput = harness.fixture.nativeElement.querySelector('input[id="target-audience"]');
+    expect(classTargetAudienceInput).toBeTruthy();
+  });
+
+  it('should have a create class button', () => {
+    const createClassButton = harness.fixture.nativeElement.querySelector('button[type="submit"]');
+    expect(createClassButton).toBeTruthy();
+  });
+
 });
