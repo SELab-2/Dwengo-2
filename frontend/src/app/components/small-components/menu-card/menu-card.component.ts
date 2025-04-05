e7c5aa1ceefb5c2600d11325bb518e7aa8d8c24e
lennertdr <lennertderouck@gmail.com>
2025-04-05 12:59:50 +0200

    updated menu card to angular material

--- frontend/src/app/components/small-components/menu-card/menu-card.component.ts
+++ frontend/src/app/components/small-components/menu-card/menu-card.component.ts
@@ -1,15 +1,16 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
+import { MatRippleModule } from '@angular/material/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-menu-card',
     standalone: true,
-    imports: [CommonModule, MatIconModule],
+    imports: [CommonModule, MatIconModule, MatCardModule, MatRippleModule],
     templateUrl: './menu-card.component.html',
     styleUrls: ['./menu-card.component.less']
 })
-
 export class MenuCardComponent {
     @Input() title!: string;
     @Input() count!: number;
@@ -18,12 +19,11 @@ export class MenuCardComponent {
     @Input() color!: string;
     @Output() cardClick = new EventEmitter<void>();
 
-    getColor() {
-        return this.color ? this.color : "blue"
+    getColorClass(): string {
+        return this.color ? this.color : "blue";
     }
 
     onClick() {
         this.cardClick.emit();
     }
 }
-
