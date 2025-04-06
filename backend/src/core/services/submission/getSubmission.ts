c9396bedd70bd43551ed544132c939682f3386aa
AdrienVL <adrien.vanlancker@ugent.be>
2025-04-06 19:57:58 +0200

    feat: submission error handling

--- backend/src/core/services/submission/getSubmission.ts
+++ backend/src/core/services/submission/getSubmission.ts
@@ -1,16 +1,25 @@
 import { z } from "zod";
 import { SubmissionBaseService } from "./submissionBaseService";
 import { getSubmissionSchema } from "../../../application/schemas/submissionSchemas";
+import { tryRepoEntityOperation } from "../../helpers";
 
 export type GetSubmissionInput = z.infer<typeof getSubmissionSchema>;
 
 export class GetSubmission extends SubmissionBaseService<GetSubmissionInput> {
+    /**
+     * Executes the submission get process.
+     * @param input - The input data for getting a submission, validated by getSubmissionSchema.
+     * @returns A promise resolving to a submission transformed into an object.
+     * @throws {ApiError} If the submission with the given id was not found.
+     */
     async execute(input: GetSubmissionInput): Promise<object> {
-        const submission = await this.submissionRepository.getById(input.id);
-        return { id: submission.id };
+        return (
+            await tryRepoEntityOperation(this.submissionRepository.getById(input.id), "Submission", input.id, true)
+        ).toObject();
     }
 }
 
+// TODO - don't forget to add the try after PR is resolved
 // TODO export type GetUsertSubmissionsInput = z.infer<typeof getUserSubmissionsSchema>;
 //
 // TODO export class GetUserSubmissions extends SubmissionBaseService<GetUsertSubmissionsInput> {
