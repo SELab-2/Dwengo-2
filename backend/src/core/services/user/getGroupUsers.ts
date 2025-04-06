631f5e748829f3d182447ce551de8e4be59684c0
AdrienVL <adrien.vanlancker@ugent.be>
2025-04-06 19:59:09 +0200

    feat: user error handling

--- backend/src/core/services/user/getGroupUsers.ts
+++ backend/src/core/services/user/getGroupUsers.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import { getGroupUsersSchema } from "../../../application/schemas/userSchemas";
 import { Service } from "../../../config/service";
+import { Student } from "../../entities/student";
+import { tryRepoEntityOperation } from "../../helpers";
 import { IStudentRepository } from "../../repositories/studentRepositoryInterface";
 
 export type GetGroupUsersInput = z.infer<typeof getGroupUsersSchema>;
@@ -8,8 +10,19 @@ export type GetGroupUsersInput = z.infer<typeof getGroupUsersSchema>;
 export class GetGroupUsers implements Service<GetGroupUsersInput> {
     constructor(private studentRepository: IStudentRepository) {}
 
+    /**
+     * Executes the group users get process.
+     * @param input - The input data for getting group users, validated by getGroupUsersSchema.
+     * @returns A promise resolving to an object with a list of users.
+     * @throws {ApiError} If the group with the given id is not found.
+     */
     async execute(input: GetGroupUsersInput): Promise<object> {
-        const studentIds = (await this.studentRepository.getByGroupId(input.idParent)).map(s => s.id);
-        return { students: studentIds };
+        const students: Student[] = await tryRepoEntityOperation(
+            this.studentRepository.getByGroupId(input.idParent),
+            "Group",
+            input.idParent,
+            true,
+        );
+        return { students: students.map(s => s.id) };
     }
 }
