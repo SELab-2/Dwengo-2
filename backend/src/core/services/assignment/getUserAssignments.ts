d6fb24dd260930007581701bb1f556821f31d113
AdrienVL <adrien.vanlancker@ugent.be>
2025-04-06 19:52:01 +0200

    feat: assignment error handling

--- backend/src/core/services/assignment/getUserAssignments.ts
+++ backend/src/core/services/assignment/getUserAssignments.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import { AssignmentService } from "./assignmentService";
 import { getUserAssignmentsSchema } from "../../../application/schemas/assignmentSchemas";
 import { Assignment } from "../../entities/assignment";
+import { tryRepoEntityOperation } from "../../helpers";
 
 export type GetUserAssignmentsInput = z.infer<typeof getUserAssignmentsSchema>;
 
@@ -9,8 +10,19 @@ export type GetUserAssignmentsInput = z.infer<typeof getUserAssignmentsSchema>;
  * Service class to get all assignments of a user.
  */
 export class GetUserAssignments extends AssignmentService<GetUserAssignmentsInput> {
+    /**
+     * Executes the user assignments get process.
+     * @param input - The input data for getting user assignments, validated by getUserAssignmentsSchema.
+     * @returns A promise resolving to an object with a list of assignments.
+     * @throws {ApiError} If the user with the given id is not found.
+     */
     async execute(input: GetUserAssignmentsInput): Promise<object> {
-        const assignments: Assignment[] = await this.assignmentRepository.getByUserId(input.idParent);
+        const assignments: Assignment[] = await tryRepoEntityOperation(
+            this.assignmentRepository.getByUserId(input.idParent),
+            "User",
+            input.idParent,
+            true,
+        );
         return { assignments: assignments.map(assignment => assignment.id) };
     }
 }
