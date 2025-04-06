aef8601a888b66e38151ed9675c7170dee072658
brentjan <brentjan.janssens@ugent.be>
2025-04-06 13:06:41 +0200

    chore: init class members list component

---
+++ frontend/src/app/components/class-members-list/class-members-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-class-members-list',
+  imports: [],
+  templateUrl: './class-members-list.component.html',
+  styleUrl: './class-members-list.component.less'
+})
+export class ClassMembersListComponent {
+
+}
