6f7072a0df852d4f932c206ad13671cae5c41043
rvdkeere <robbe.vandekeere@ugent.be>
2025-04-06 16:18:38 +0200

    Update student-dashboard.component.ts
    cleanup of mock data version

--- frontend/src/app/components/dashboard/student-dashboard.component.ts
+++ frontend/src/app/components/dashboard/student-dashboard.component.ts
@@ -2,96 +2,50 @@ import { CommonModule } from '@angular/common';
 import { Component, ViewChild, Inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { MatList } from '@angular/material/list'
 import { AssignmentsService } from '../../services/assignments.service';
 import { AuthenticationService } from '../../services/authentication.service';
 import { Assignment } from '../../interfaces/assignments/assignment';
 import { ErrorService } from '../../services/error.service';
-
-import { NewAssignment } from '../../interfaces/assignments/newAssignment';
-import { NewClass } from '../../interfaces/classes/newClass';
-import { ClassesService } from '../../services/classes.service';
+import { MiniAssignmentComponent } from '../mini-assignment/mini-assignment.component';
 
 
 @Component({
   selector: 'app-student-dashboard',
   standalone: true,
-  imports: [RouterLink, CommonModule, MatIconModule],
+  imports: [RouterLink, CommonModule, MatIconModule, MatList, MiniAssignmentComponent],
   templateUrl: './student-dashboard.component.html',
   styleUrls: ['./student-dashboard.component.less'],
-//   providers: [StudentDashboardService]
+  // providers: [StudentDashboardService]
 })
 export class StudentDashboardComponent implements OnInit {
-
-  // id: string = "test";
-  // learningPathsLink: string = '/student/learning-paths';
-
-  // selectedView: string | null = null;
+  private _assignments: Assignment[] = [];
 
   constructor(
-      private authService: AuthenticationService,
-      private errorService: ErrorService,
+      // private authService: AuthenticationService,
+      // private errorService: ErrorService,
       private assignmentsService: AssignmentsService,
-      private _assignments: Assignment[] = [],
-
-      // private classesService: ClassesService,
-      // private newClass: NewClass = {} as NewClass,
-      // private newAssignment: NewAssignment = {} as NewAssignment,
-      // private classId: string = '',
-      // private assignmentId: string = '',
     ) {}
   
   ngOnInit() {
-     //TODO remove this test code
-    // this.newClass = {
-    //   name: "math",
-    //   description: "math class",
-    //   targetAudience: "students",
-    //   teacherId: this.authService.retrieveUserId()?.toString() || '',
-    // };
-
-    // this.classesService.createClass(this.newClass).subscribe(
-    //     this.errorService.subscribeHandler("An error occured, please try again",
-    //         (id) => {
-    //             this.classId = id;
-    //             console.log("Class created with ID: ", id);
-    //         }
-    //     )); //TODO remove this test code
-    
-    // this.newAssignment = {
-    //   classId: this.classId,
-    //   learningPathId: "test",
-    //   startDate: "2023-10-01",
-    //   deadline: "2023-10-31",
-    //   extraInstructions: "test123",
-    // }; //TODO remove this test code
-
-    // this.assignmentsService.createAssignment(this.newAssignment).subscribe(
-    //     this.errorService.subscribeHandler("An error occured, please try again",
-    //         (id) => {
-    //             this.assignmentId = id;
-    //             console.log("Assignment created with ID: ", id);
-    //         }
-    //     )); //TODO remove this test code
-
-
-    this.assignmentsService
-      .assignmentsOfUser()
-      .subscribe({
-        next: (assignments) => this._assignments = assignments,
-      });
+    // this.assignmentsService
+    //   .assignmentsOfUser()
+    //   .subscribe({
+    //     next: (assignments) => this._assignments = assignments,
+    //   });
     
     this._assignments = [ //TODO remove this test code
       {
-        id: "1",
-        classId: "1",
+        id: "Assignment1",
+        classId: "class 1",
         learningPathId: "1",
         startDate: "2023-10-01",
         deadline: "2023-10-31",
         extraInstructions: "test123",
       },
       {
-        id: "2",
-        classId: "2",
+        id: "Trivia Quiz",
+        classId: "class 2",
         learningPathId: "2",
         startDate: "2023-11-01",
         deadline: "2023-11-15",
@@ -100,7 +54,8 @@ export class StudentDashboardComponent implements OnInit {
     ]
   }
 
-  // setView(view: string) {
-  //   this.selectedView = this.selectedView === view ? null : view;
-  // }
+  
+    public get assignments(): Assignment[] {
+      return this._assignments;
+    }
 }
