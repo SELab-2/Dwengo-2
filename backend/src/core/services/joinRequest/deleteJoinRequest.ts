9d5e741398129f52d2c2b619138ca9bbd3600dcb
AdrienVL <adrien.vanlancker@ugent.be>
2025-04-06 19:56:01 +0200

    feat: join-request error handling

--- backend/src/core/services/joinRequest/deleteJoinRequest.ts
+++ backend/src/core/services/joinRequest/deleteJoinRequest.ts
@@ -1,12 +1,19 @@
 import { z } from "zod";
 import { JoinRequestService } from "./joinRequestService";
 import { deleteJoinRequestSchema } from "../../../application/schemas";
+import { tryRepoEntityOperation } from "../../helpers";
 
 export type DeleteJoinRequestInput = z.infer<typeof deleteJoinRequestSchema>;
 
 export class DeleteJoinRequest extends JoinRequestService<DeleteJoinRequestInput> {
+    /**
+     * Executes the group deletion process.
+     * @param input - The input data for deleting a group, validated by deleteGroupSchema.
+     * @returns An empty object.
+     * @throws {ApiError} If the group with the given id is not found.
+     */
     async execute(input: DeleteJoinRequestInput): Promise<object> {
-        await this.joinRequestRepository.delete(input.id);
+        await tryRepoEntityOperation(this.joinRequestRepository.delete(input.id), "JoinRequest", input.id, true);
         return {};
     }
 }
