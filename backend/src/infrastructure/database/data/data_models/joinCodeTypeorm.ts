c8581b2a47fe33fc09052ccf94c5c802748b6b48
Briek <briek.rombaut@ugent.be>
2025-04-07 08:52:39 +0200

    fixed the <BeforeInsert is not a function> issue

--- backend/src/infrastructure/database/data/data_models/joinCodeTypeorm.ts
+++ backend/src/infrastructure/database/data/data_models/joinCodeTypeorm.ts
@@ -1,11 +1,9 @@
-import { Entity, PrimaryColumn, Column, ManyToOne, BeforeInsert, CreateDateColumn } from "typeorm";
+import { Entity, PrimaryColumn, Column, ManyToOne, CreateDateColumn } from "typeorm";
+import { BeforeInsert } from "typeorm/decorator/listeners/BeforeInsert" // Important to specify the exact path here
 import { ClassTypeORM } from "./classTypeorm";
 
 @Entity()
 export class JoinCodeTypeORM {
-    @PrimaryColumn({ unique: true })
-    code!: string; // a short id of length 6 with alphanumeric characters
-
     @BeforeInsert()
     generateShortId() {
         // Convert a random number to its base 36 representation. (uses 0-9 and a-z)
@@ -13,6 +11,9 @@ export class JoinCodeTypeORM {
         // Skip the "0.", use the decimals to make the id
         this.code = base36String.substring(2, 8);
     }
+
+    @PrimaryColumn({ unique: true })
+    code!: string; // a short id of length 6 with alphanumeric characters
 
     @ManyToOne(() => ClassTypeORM, { cascade: true, onDelete: "CASCADE" })
     class!: ClassTypeORM;
