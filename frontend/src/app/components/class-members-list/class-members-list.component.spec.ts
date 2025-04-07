0a637c1bd8d9825734f03e8fe0e4351124a9538f
brentjan <brentjan.janssens@ugent.be>
2025-04-07 13:05:54 +0200

    chore: commit before checkout

--- frontend/src/app/components/class-members-list/class-members-list.component.spec.ts
+++ frontend/src/app/components/class-members-list/class-members-list.component.spec.ts
@@ -3,21 +3,5 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ClassMembersListComponent } from './class-members-list.component';
 
 describe('ClassMembersListComponent', () => {
-  // let component: ClassMembersListComponent;
-  // let fixture: ComponentFixture<ClassMembersListComponent>;
 
-  // beforeEach(async () => {
-  //   await TestBed.configureTestingModule({
-  //     imports: [ClassMembersListComponent]
-  //   })
-  //   .compileComponents();
-
-  //   fixture = TestBed.createComponent(ClassMembersListComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
-
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
 });
