13bf891652f3d6c82ec98c78e0afa6a26714e00c
thomasdejaeghere <thomas.dejaeghere@ugent.be>
2025-04-11 01:31:59 +0200

    datasingleton test is a pain in the ass

--- backend/jest.unit.setup.ts
+++
@@ -1,5 +0,0 @@
-import { DatasourceTypeORMSingleton } from "./src/infrastructure/database/data/data_sources/typeorm/datasourceTypeORMSingleton"
-
-afterEach(async () => {
-    await DatasourceTypeORMSingleton.shutdownDatabase();
-})
