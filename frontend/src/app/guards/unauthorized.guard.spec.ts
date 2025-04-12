cc9099208381950f2121d34677bb4f1330c7344a
Bram Comyn <bram.comyn@ugent.be>
2025-04-12 16:09:19 +0200

    test: guards

--- frontend/src/app/guards/unauthorized.guard.spec.ts
+++ frontend/src/app/guards/unauthorized.guard.spec.ts
@@ -1,6 +1,45 @@
-import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { TestBed } from "@angular/core/testing";
+import { Router } from "@angular/router";
+import { UnauthorizedGuard } from "./unauthorized.guard";
+import { AuthenticationService } from "../services/authentication.service";
 
-import { UnauthorizedGuard } from './unauthorized.guard';
+describe('UnauthorizedGuard', () => {
+    let guard: UnauthorizedGuard;
+    let authenticationService: jasmine.SpyObj<AuthenticationService>;
+    let routerSpy: jasmine.SpyObj<Router>;
 
+    beforeEach(() => {
+        authenticationService = jasmine.createSpyObj('AuthenticationService', ['retrieveUserId']);
+        routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
 
+        TestBed.configureTestingModule({
+            providers: [
+                UnauthorizedGuard,
+                { provide: AuthenticationService, useValue: authenticationService },
+                { provide: Router, useValue: routerSpy }
+            ]
+        });
+
+        guard = TestBed.inject(UnauthorizedGuard);
+    });
+
+    it('should be created', () => {
+        expect(guard).toBeTruthy();
+    });
+
+    it('should allow access if user is not logged in', () => {
+        authenticationService.retrieveUserId.and.returnValue(null);
+
+        const result = guard.canActivate();
+        expect(result).toBeTrue();
+        expect(routerSpy.navigateByUrl).not.toHaveBeenCalled();
+    });
+
+    it('should deny access and navigate if user is logged in', () => {
+        authenticationService.retrieveUserId.and.returnValue("someUserId");
+
+        const result = guard.canActivate();
+        expect(result).toBeFalse();
+        expect(routerSpy.navigateByUrl).toHaveBeenCalledWith('already-authenticated');
+    });
+});
