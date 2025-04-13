d00480fce92cb611f24f410fa80786e00cc5515d
AdrienVL <adrien.vanlancker@ugent.be>
2025-04-13 21:31:46 +0200

    feat: updated join code repo interface

--- backend/src/core/repositories/joinCodeRepositoryInterface.ts
+++ backend/src/core/repositories/joinCodeRepositoryInterface.ts
@@ -1,4 +1,5 @@
 import { AbstractRepository } from "./abstractRepository";
+import { JoinCode } from "../../core/entities/joinCode";
 
 /**
  * Interface for join code repositories.
@@ -6,18 +7,39 @@ import { AbstractRepository } from "./abstractRepository";
  */
 export abstract class IJoinCodeRepository extends AbstractRepository {
     /**
-     * Get an active join code for a class.
-     * If no active join code exists yet,
-     * One is created and returned
+     * Creates a new join code for a class.
      * @param classId The id of the class.
-     * @returns A promise that resolves to the alphanumeric 6 letter join code.
+     * @returns A promise that resolves to the created JoinCode entity.
      */
-    public abstract getByClassId(classId: string): Promise<string>;
+    public abstract create(classId: string): Promise<JoinCode>;
+
+    /**
+     * Get a join code by its code.
+     * @param code The alphanumeric join code.
+     * @returns A promise that resolves to the JoinCode entity.
+     * @throws EntityNotFoundError if the join code is not found.
+     */
+    public abstract getById(code: string): Promise<JoinCode>;
+
+    /**
+     * Get an active join code for a class.
+     * If no active join code exists yet, one is created and returned.
+     * @param classId The id of the class.
+     * @returns A promise that resolves to the alphanumeric 6-letter join code.
+     */
+    public abstract getByClassId(classId: string): Promise<JoinCode[]>;
 
     /**
      * Marks a join code as expired.
-     * No students will be able to use this join code after the code is expired.
-     * @param code The actual alphanumerical code as a string.
+     * No students will be able to use this join code after it is expired.
+     * @param code The alphanumeric join code.
+     * @throws EntityNotFoundError if the join code is not found.
      */
     public abstract setExpired(code: string): Promise<void>;
+
+    /**
+     * Deletes a join code.
+     * @param code The alphanumeric join code.
+     */
+    public abstract delete(code: string): Promise<void>;
 }
