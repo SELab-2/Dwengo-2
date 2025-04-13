aab34aa8b59f4707fc83c13c4c7dd45ca82c9cb2
Bram Comyn <bram.comyn@ugent.be>
2025-04-13 14:12:36 +0200

    feat: updated routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,11 +5,11 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
 import { ClassesPageComponent } from './pages/classes-page/classes-page.component';
 import { ClassComponent } from './components/class/class.component';
 import { UnknownRouteComponent } from './components/unknown-route/unknown-route.component';
-import { StudentAssignmentComponent } from './pages/student-assignment/student-assignment.component';
 import { UserTypeGuard } from './guards/usertype.guard';
 import { UnauthorizedComponent } from './components/unauthorized/unauthorized.component';
 import { UnauthorizedGuard } from './guards/unauthorized.guard';
 import { AlreadyAuthenticatedComponent } from './components/already-authenticated/already-authenticated.component';
+import { AssignmentOversightComponent } from './components/assignment-oversight/assignment-oversight.component';
 
 /**
  * Routing of our frontend.
@@ -21,7 +21,8 @@ export const routes: Routes = [
     { path: 'teacher/classes/:id', component: ClassComponent, title: "Class", canActivate: [UserTypeGuard], data: { userType: 'teacher' } },
     { path: 'student/classes', component: ClassesPageComponent, title: "My Classes", canActivate: [UserTypeGuard], data: { userType: 'student' } },
     { path: 'student/classes/:id', component: ClassComponent, title: "Class", canActivate: [UserTypeGuard], data: { userType: 'student' } },
-    { path: 'student/assignments', component: StudentAssignmentComponent, title: "My Assignments", canActivate: [UserTypeGuard], data: { userType: 'student' } },
+    { path: 'student/assignments', component: AssignmentOversightComponent, title: "My Assignments", canActivate: [UserTypeGuard], data: { userType: 'student' } },
+    { path: 'teacher/assignments', component: AssignmentOversightComponent, title: "My Assignments", canActivate: [UserTypeGuard], data: { userType: 'teacher' } },
     { path: 'teacher/login', component: LoginPageComponent, title: 'Teacher Login', data: { isTeacher: true }, canActivate: [UnauthorizedGuard] },
     { path: 'student/login', component: LoginPageComponent, title: 'Student Login', data: { isTeacher: false }, canActivate: [UnauthorizedGuard] },
     { path: 'register', component: RegisterPageComponent, title: 'Register', canActivate: [UnauthorizedGuard] },
