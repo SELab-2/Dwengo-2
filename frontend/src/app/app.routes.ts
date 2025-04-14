94c6a588ff7a8b3205afc75900145ad8002911a6
rvdkeere <robbe.vandekeere@ugent.be>
2025-04-14 16:12:38 +0200

    fix package lock & routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -19,10 +19,10 @@ import { AlreadyAuthenticatedComponent } from './components/already-authenticate
  */
 export const routes: Routes = [
     { path: '', component: LandingPageComponent, title: 'Landing Page', canActivate: [UnauthorizedGuard] },
-    { path: 'student/dashboard', redirectTo: 'student/classes' },
     { path: 'teacher/dashboard', component: TeacherDashboardPageComponent, canActivate: [UserTypeGuard], data: { userType: 'teacher' } },
     { path: 'teacher/classes', component: ClassesPageComponent, title: "My Classes", canActivate: [UserTypeGuard], data: { userType: 'teacher' } },
     { path: 'teacher/classes/:id', component: ClassComponent, title: "Class", canActivate: [UserTypeGuard], data: { userType: 'teacher' } },
+    { path: 'student/dashboard', component: StudentDashboardPageComponent, canActivate: [UserTypeGuard], data: { userType: 'student' } },
     { path: 'student/classes', component: ClassesPageComponent, title: "My Classes", canActivate: [UserTypeGuard], data: { userType: 'student' } },
     { path: 'student/classes/:id', component: ClassComponent, title: "Class", canActivate: [UserTypeGuard], data: { userType: 'student' } },
     { path: 'student/assignments', component: StudentAssignmentComponent, title: "My Assignments", canActivate: [UserTypeGuard], data: { userType: 'student' } },
