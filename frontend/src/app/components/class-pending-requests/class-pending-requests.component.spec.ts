0ae8d04834652fd612510a9e45cb6a3f83977bbd
brentjan <brentjan.janssens@ugent.be>
2025-04-14 18:21:44 +0200

    chore: tests for components

--- frontend/src/app/components/class-pending-requests/class-pending-requests.component.spec.ts
+++ frontend/src/app/components/class-pending-requests/class-pending-requests.component.spec.ts
@@ -1,20 +1,63 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ClassPendingRequestsComponent } from "./class-pending-requests.component";
+import { provideHttpClient } from "@angular/common/http";
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { Class } from "../../interfaces/classes/class";
+import { environment } from "../../../environments/environment";
 
-// describe('ClassPendingRequestsComponent', () => {
-//   // let component: PendingRequestsComponent;
-//   // let fixture: ComponentFixture<PendingRequestsComponent>;
 
-//   // beforeEach(async () => {
-//   //   await TestBed.configureTestingModule({
-//   //     imports: [PendingRequestsComponent]
-//   //   })
-//   //   .compileComponents();
+describe('ClassPendingRequestsComponent', () => {
+  let component: ClassPendingRequestsComponent;
+  let fixture: ComponentFixture<ClassPendingRequestsComponent>;
+  let httpTest: HttpTestingController;
 
-//   //   fixture = TestBed.createComponent(PendingRequestsComponent);
-//   //   component = fixture.componentInstance;
-//   //   fixture.detectChanges();
-//   // });
+  const testClass: Class = {
+    name: "TestMath",
+    description: "This is a test class",
+    targetAudience: "Testers",
+    teacherId: "123",
+    id: "123"
+  };
+  const API_URL = environment.API_URL;
 
-//   // it('should create', () => {
-//   //   expect(component).toBeTruthy();
-//   // });
-// });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ClassPendingRequestsComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    })
+    .compileComponents();
+
+    httpTest = TestBed.inject(HttpTestingController);
+
+    fixture = TestBed.createComponent(ClassPendingRequestsComponent);
+    component = fixture.componentInstance;
+    component._class = testClass;
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should make request to join requests of class', () => {
+    // Make request to requests of a class
+    const requestToClassRequests = httpTest.expectOne(`${API_URL}/classes/${testClass.id}/requests`);
+    expect(requestToClassRequests.request.method).toBe('GET');
+
+    const requesterId: string = "12345";
+    requestToClassRequests.flush([
+      {
+        id: "requestId",
+        requester: requesterId,
+        classId: testClass.id,
+        type: "student"
+      }
+    ]);
+  });
+
+  
+});
