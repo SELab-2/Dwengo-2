9c48371c06edc366f981ca78df54f841fe68201a
Briek <briek.rombaut@ugent.be>
2025-04-21 14:51:56 +0200

    changed hard coded conversion from entity to typeorm to use the createTypeORM method instead.

--- backend/src/infrastructure/database/data/data_sources/typeorm/datasourceAssignmentTypeORM.ts
+++ backend/src/infrastructure/database/data/data_sources/typeorm/datasourceAssignmentTypeORM.ts
@@ -20,14 +20,10 @@ export class DatasourceAssignmentTypeORM extends DatasourceTypeORM {
         }
 
         // Class exists and teacher exist: insert assignment into the database
-        const assignmentModel: AssignmentTypeORM = datasource.getRepository(AssignmentTypeORM).create({
-            class: { id: newAssignment.classId },
-            learning_path_id: newAssignment.learningPathId,
-            start: newAssignment.startDate,
-            deadline: newAssignment.deadline,
-            name: newAssignment.name,
-            extra_instructions: newAssignment.extraInstructions,
-        });
+        let assignmentModel = AssignmentTypeORM.createTypeORM(newAssignment, classModel);
+        assignmentModel = datasource.getRepository(AssignmentTypeORM).create(
+            assignmentModel
+        );
 
         await datasource.getRepository(AssignmentTypeORM).save(assignmentModel);
 
