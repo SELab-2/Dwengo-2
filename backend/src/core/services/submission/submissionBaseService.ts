2f89264db8db6470b442f936f6ed60361a80b4b3
Maarten Lambrecht <maalambr.lambrecht@ugent.be>
2025-04-22 14:29:31 +0200

    Docs+test: Update swagger + add new way to get all submissions for assignment of a user + test

--- backend/src/core/services/submission/submissionBaseService.ts
+++ backend/src/core/services/submission/submissionBaseService.ts
@@ -1,6 +1,9 @@
 import { Service } from "../../../config/service";
 import { ISubmissionRepository } from "../../repositories/submissionRepositoryInterface";
 
+/**
+ * Base class for submission services.
+ */
 export abstract class SubmissionBaseService<T> implements Service<T> {
     constructor(protected submissionRepository: ISubmissionRepository) {}
     abstract execute(input: T): Promise<object>;
