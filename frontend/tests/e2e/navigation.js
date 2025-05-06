4ee1567ae185a1c8cf9717789d3895b1dbcb064e
brentjan <brentjan.janssens@ugent.be>
2025-05-06 17:41:39 +0200

    feat: wrote actual test

--- frontend/tests/e2e/navigation.js
+++ frontend/tests/e2e/navigation.js
@@ -2,7 +2,7 @@ describe('Navigation', function () {
 
     it('should load the page and check the title', async function (browser) {
         browser
-            .url(browser.baseUrl)
+            .url(browser.baseUrl) // https://nightwatchjs.org/guide/reference/settings.html#postdoc-setting-the-baseurl-property
             .assert.titleContains('Landing Page')
             .end();
     });
