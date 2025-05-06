a9f13f851c82d1d201e2d6ef918b64a1a68019a9
Maarten Lambrecht <maalambr.lambrecht@ugent.be>
2025-05-06 20:08:15 +0200

    Fix: Try again with chromium via snap, chromedriver only wants this version

--- frontend/nightwatch.conf_server.js
+++ frontend/nightwatch.conf_server.js
@@ -60,7 +60,7 @@ module.exports = {
       desiredCapabilities: {
         browserName: 'chrome',
         'goog:chromeOptions': {
-          binary: '/usr/bin/google-chrome-stable',
+          binary: '/snap/bin/chromium',
           args: [ 
             '--headless',
             // Extra options because otherwise get that the ChromeDriver cannot create a session
