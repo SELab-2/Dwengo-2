1b85dd379a1780eafc196a4a9da82869beb0d17f
brentjan <brentjan.janssens@ugent.be>
2025-05-09 10:10:37 +0200

    feat: landing page e2e test

---
+++ frontend/tests/e2e/landingPageTest.js
@@ -0,0 +1,42 @@
+describe('Landing page', function () {
+
+    beforeEach(() => {
+        // Browser to: http://localhost:4200
+        browser.url(browser.baseUrl);
+    });
+
+    it('should load the page and contain title: "Landing Page"', async function (browser) {
+        expect(browser.getTitle()).contain('Landing Page');
+    });
+
+    it('should route to register page after clicking "Register"', async function (browser) {
+        // Click on register
+        browser.click('button[name=register-button]');
+
+        expect(browser.getTitle()).contain('Register');
+    });
+
+    it('should route to teacher login page after clicking "Teacher login"', async function (browser) {
+        // Click on teacher login
+        browser.click('button[name=teacher-login-button]');
+
+        expect(browser.getTitle()).contain('Teacher Login');
+    });
+
+    it('should route to student login page after clicking "Student login"', async function (browser) {
+        // Click on student
+        browser.click('button[name=student-login-button]');
+
+        expect(browser.getTitle()).contain('Student Login');
+    });
+
+    it('should have a dwengo logo with an alt text', async function (browser) {
+        // TODO: toBeDefined() is not a function when using `expect`
+        browser
+            .element
+            .findByAltText('Dwengo logo')
+            .assert
+            .visible();
+    });
+
+});
