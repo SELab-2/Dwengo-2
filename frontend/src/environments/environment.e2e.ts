7eb682e427d2063eadfcb2091aff89c987bdb72e
brentjan <brentjan.janssens@ugent.be>
2025-05-11 18:16:27 +0200

    feat: changed ci.yml

---
+++ frontend/src/environments/environment.e2e.ts
@@ -0,0 +1,9 @@
+interface Environment {
+    production: boolean;
+    API_URL: string;
+}
+
+export const environment: Environment = {
+    production: false,
+    API_URL: 'http://dwengo-2-backend-1:3000'
+}
