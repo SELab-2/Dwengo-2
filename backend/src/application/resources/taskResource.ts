e6c22de2fd428ce7d7d31f235d2fae6f728db27b
thomasdejaeghere <thomas.dejaeghere@ugent.be>
2025-05-16 18:43:06 +0200

    updated docstring for taskresource

--- backend/src/application/resources/taskResource.ts
+++ backend/src/application/resources/taskResource.ts
@@ -8,12 +8,11 @@ import * as TaskSchemas from "../schemas/taskSchemas";
  * converting Express request/response objects to our internal format.
  *
  * Supported endpoints:
- * - GET /users/:idParent/task - Get the task of all assignments of a user.
- * - GET /assignments/:idParent/task - Get the task of all users in an assignment
- * - GET /groups/:idParent/task - Get the task of all users in a group
- * - GET /users/:userId/assignment/:assignmentId/task - Get the task of a user for a specific assignment
- * - GET /classes/:idParent/completion - Shows how much of the assignments for a class have been completed (on average)
- * - GET /classes/:idParent/activity - Get an array with the amount of submissions for a class in the last 12 months (array of zero to twelve numbers)
+ * - GET /tasks/:id - Get a task with id
+ * - PATCH /tasks/:id - Update a task with id
+ * - DELETE /tasks/:id - Delete a task with id
+ * - POST /tasks - Create a task
+ * - GET /assignments/:idParent/tasks?step=number - Get all tasks of an assignment (with optional step selection)
  */
 
 /* ************* Extractors ************* */
