abbd20a7c28c1925d88a507da6f2b6315f5254ec
thomasdejaeghere <thomas.dejaeghere@ugent.be>
2025-05-20 00:59:49 +0200

    worked on styling

--- frontend/src/app/components/small-components/assignment-card/assignment-card.component.ts
+++ frontend/src/app/components/small-components/assignment-card/assignment-card.component.ts
@@ -16,7 +16,7 @@ import { UserType } from "../../../interfaces";
     standalone: true,
     imports: [CommonModule, MatIconModule, MatCardModule, MatRippleModule, MatButtonModule, MatTooltipModule, MatIcon],
     templateUrl: './assignment-card.component.html',
-    styleUrls: ['./assignment-card.component.less']
+    styleUrls: ['./assignment-card.component.scss']
 })
 export class AssignmentCardComponent implements OnInit {
     @Input() assignment!: Assignment;
